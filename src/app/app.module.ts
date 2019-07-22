057f03c1b54d2f231bb66bca80769b1432942300
Michael Nguyen <mqvnguyen9@gmail.com>
2019-07-21 22:03:11 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TunerComponent } from './tuner/tuner.component';
+import {AudioContextModule} from 'angular-audio-context';
+import {AudioContextProxy} from 'angular-audio-context/build/es2018/audio-context-proxy';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonToggleModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TunerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AudioContextModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule
   ],
-  providers: [],
+  providers: [AudioContextProxy],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
